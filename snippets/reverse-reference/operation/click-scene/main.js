616b978363d0ff15ac02ff7f6accd50b553e2c29
jenkins <akashic@dwango.co.jp>
2021-02-01 12:08:57 +0900

    udpate akashic-web-page

---
+++ snippets/reverse-reference/operation/click-scene/main.js
@@ -0,0 +1,34 @@
+function main() {
+	var scene = new g.Scene({ game: g.game });
+	scene.onLoad.add(function() {
+		var rect = new g.FilledRect({
+			scene: scene,
+			cssColor: "red",
+			width: 100,
+			height: 100,
+			touchable: true
+		});
+		scene.append(rect);
+
+		rect.onPointDown.add(function() {
+			rect.cssColor = rect.cssColor === "red" ? "blue" : "red";
+			rect.modified();
+		});
+	});
+	scene.onPointDownCapture.add((ev) => {
+		var size = 20;
+		var rect = new g.FilledRect({
+			scene: scene,
+			x: ev.point.x - size / 2,
+			y: ev.point.y - size / 2,
+			width: size,
+			height: size,
+			cssColor: "black"
+		});
+		scene.append(rect);
+	});
+	g.game.pushScene(scene);
+}
+
+
+module.exports = main;
---
+++ snippets/reverse-reference/text/bitmapfont-use/main.js
@@ -0,0 +1,28 @@
+function main() {
+	var scene = new g.Scene({
+		game: g.game,
+		assetIds: ["mplus", "mplus-glyph"]
+	});
+
+	scene.onLoad.add(function() {
+		var mPlusGlyphInfo = JSON.parse(scene.assets["mplus-glyph"].data)
+		var mplusfont = new g.BitmapFont({
+			src: scene.assets["mplus"],
+			glyphInfo: mPlusGlyphInfo
+		});
+
+		var label = new g.Label({
+			scene: scene,
+			font: mplusfont,
+			text: "こんにちは",
+			fontSize: 48,
+			x: 100,
+			y: 50
+		});
+		scene.append(label);
+	});
+
+	g.game.pushScene(scene);
+}
+
+module.exports = main;
